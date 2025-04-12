3462e60fa31261d3bcb593656c5b99fcbee4a30c
amaralis <artem404304@gmail.com>
2025-04-12 15:45:18 +0300

    add repository

---
+++ src/main/java/ru/example/edu/repository/StudentRepository.java
@@ -0,0 +1,6 @@
+package ru.example.edu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.example.edu.model.Student;
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
